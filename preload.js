5764c6499fd89363a8e5e86d0e72246e712bc61a
beckettonfilm-sys <beckettonfilm@gmail.com>
2025-12-04 21:06:24 +0100

    Delete preload.js

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("electronAPI", {
-  fetchWorkbook: () => ipcRenderer.invoke("fetch-workbook"),
-  updateWorkbook: (records, sheetName) =>
-    ipcRenderer.invoke("update-workbook", { records, sheetName })
-});
