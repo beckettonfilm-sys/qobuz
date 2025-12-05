b2b6a7ae1785a869c2b4f0c67780adbecd346b11
beckettonfilm-sys <beckettonfilm@gmail.com>
2025-12-05 14:29:09 +0100

    Add files via upload

---
+++ preload_13.js
@@ -0,0 +1,7 @@
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("electronAPI", {
+  fetchWorkbook: () => ipcRenderer.invoke("fetch-workbook"),
+  updateWorkbook: (records, sheetName) =>
+    ipcRenderer.invoke("update-workbook", { records, sheetName })
+});
